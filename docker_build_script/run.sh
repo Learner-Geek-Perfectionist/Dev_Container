1324f11b0a44f492464b6e881a108d7a2d81cd6c
ouyangzhaoxin <1273730703@qq.com>
2024-09-30 02:54:27 +0800

    update Dockerfile

--- docker_build_script/run.sh
+++ docker_build_script/run.sh
@@ -7,9 +7,14 @@ echo "Build and run Docker container..."
 # 获取上一级目录的绝对路径
 PARENT_DIR="$(cd .. && pwd)"
 
+# 构建Docker镜像
+docker build -t fedora-ssh .
+
 # 运行容器，并将上一级的 project 目录映射到容器中的 /project
+# 同时映射SSH端口，这里将容器的22端口映射到主机的2222端口
 docker run -it \
   --name fedora_test \
   -v "$PARENT_DIR/project":/project \
-  fedora \
-  /bin/bash
+  -p 2222:22 \
+  fedora-ssh \
+  /usr/sbin/sshd -D
