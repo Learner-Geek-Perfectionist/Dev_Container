bd95705cf6862ce70c70a29fad2339d64918f9ca
信萌新 <1273730703@qq.com>
2024-10-23 19:00:17 +0800

    Update run.sh

--- docker_build_script/run.sh
+++ docker_build_script/run.sh
@@ -1,6 +1,3 @@
-# 设置环境变量
-export Server_USER="oyzx"
-export Client_USER="ouyangzhaoxin"
 
 # 删除同名的容器
 echo "Stop and remove Docker container..."
@@ -17,11 +14,9 @@ PARENT_DIR="$(cd .. && pwd)"
 chmod +x $PARENT_DIR/docker_build_script/init.sh
 
 # 运行容器，并将项目 project 目录映射到容器中的 ～/project，并赋予读写权限
-# 容器的 ssh 服务的监听端口设置为 2222 ，使用 ssh -p 2222 oyzx@localhost 远程连接 docker 容器
-# 添加「客户端公钥」，实现 ssh 免密登陆。
 docker run -it \
   --name fedora_test \
   --net=host \
-  -v "$PARENT_DIR/project":/home/$Server_USER/project \
+  -v "$PARENT_DIR/project":/root/project \
   fedora \
   /bin/bash
