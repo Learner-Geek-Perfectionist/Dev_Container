3d7b74e31aea8e2bd4056d35d537d01ece2521ba
ouyangzhaoxin <1273730703@qq.com>
2024-11-18 20:25:52 +0800

    update

--- docker_build_script/ubuntu/run.sh
+++ docker_build_script/ubuntu/run.sh
@@ -10,8 +10,6 @@ echo "Build and run Docker container..."
 # 获取 run.sh 目录的上两级目录
 export PROJECT_DIR=$(dirname $(dirname $(pwd)))
 
-# 给 init.sh 添加权限
-# chmod +x $PARENT_DIR/docker_build_script/init.sh
 
 # 运行容器，并将项目 project 目录映射到容器中的 ~/project，并赋予读写权限
 docker run -it \
