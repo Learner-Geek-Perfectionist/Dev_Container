4557cc767662782b42545ba545a4d437b0bfa5a8
信萌新 <1273730703@qq.com>
2025-01-02 23:46:01 +0800

    Update run.sh

--- docker_build_script/ubuntu/run.sh
+++ docker_build_script/ubuntu/run.sh
@@ -11,8 +11,9 @@ echo "Build and run Docker container..."
 export PROJECT_DIR=$(dirname $(dirname $(pwd)))
 
 # 运行容器，并将项目 project 目录映射到容器中的 ~/project，并赋予读写权限
-docker run -it \
+docker run  \
   --name ubuntu_test \
   --net=host \
   -v "${PROJECT_DIR}/project":/root/project \
   ubuntu \
+  /bin/zsh
