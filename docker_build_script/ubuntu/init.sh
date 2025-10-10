db82e6a06be17f9409cf0f72b94f978bd0534f25
信萌新 <1273730703@qq.com>
2025-10-11 00:47:03 +0800

    update

--- docker_build_script/ubuntu/init.sh
+++ docker_build_script/ubuntu/init.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-/bin/bash -c "$(curl -fsSL "https://raw.githubusercontent.com/Learner-Geek-Perfectionist/Dotfiles/refs/heads/master/install.sh")"
+# /bin/bash -c "$(curl -fsSL "https://raw.githubusercontent.com/Learner-Geek-Perfectionist/Dotfiles/refs/heads/master/install.sh")"
+
+/bin/bash -c "$(curl -fsSL "https://raw.githubusercontent.com/Learner-Geek-Perfectionist/Dotfiles/refs/heads/master/zsh_config.sh")"
